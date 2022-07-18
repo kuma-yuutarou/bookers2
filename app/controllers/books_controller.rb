ee8fb2962f78e7e8e3d541dc4c325df1ef18e28b
EC2 Default User <ec2-user@ip-172-31-36-25.ec2.internal>
2022-07-18 04:39:58 +0000

    first commit

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,71 @@
+class BooksController < ApplicationController
+before_action :authenticate_user!
+before_action :correct_book,only: [:edit]
+
+  def correct_book
+    @book = Book.find(params[:id])
+    unless @book.user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+
+  def new
+    @books = Books.new
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+    @user = current_user
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user != current_user
+      puts 'test'
+      redirect_to books_path
+    end
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @user = current_user
+      render :index
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @bookn = Book.new
+    @user = @book.user
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
